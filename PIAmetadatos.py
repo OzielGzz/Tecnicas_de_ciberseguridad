2e16a498fc106037363a1c1717e9d55d365c15c5
DoradoMx <47581649+DoradoMx@users.noreply.github.com>
2021-11-08 14:53:29 -0600

    Agregado mta_ruta

--- PIAmetadatos.py
+++ PIAmetadatos.py
@@ -4,23 +4,39 @@ from PIL.ExifTags import TAGS as ETIQUETAS
 from tkinter.filedialog import askopenfilename
 
 def Metadatos():
-    print("Ejecutada la funcion")
-    # Explorador de archivos
-    archivo_imagen = askopenfilename()
-    foto = Imagen.open(archivo_imagen)
     contiene_metadatos = False
     datos = {}
     try:
+        # Aqui abrimos el explorador de archivos
+        archivo_imagen = askopenfilename()
+        foto = Imagen.open(archivo_imagen)
         for etiqueta, value in foto._getexif().items():
             if etiqueta in ETIQUETAS:
                 datos[ETIQUETAS[etiqueta]] = value
             
         contiene_metadatos = True
     except:
-        print("La imagen no es compatible porque no contiene metadatos")
-    #print (datos)
-    print("")
+        print ("\tERROR!\n\tPor favor revisa que la ruta sea correcta:\n",archivo_imagen,)
+        print ("\t Asegurate que el archivo este en formato JPG")
    
+    for etiqueta, value in datos.items():
+        key = ETIQUETAS.get(etiqueta, etiqueta)
+        print ("\t-" + key + ": " + str(value))
+
+def mta_ruta(ruta):
+    # Creamos una variable para avisar si hay o no metadatos
+    haveMta = False
+    datos = {}
+    try:
+        # Asignamos la ruta de la imagen JPG
+        foto = Imagen.open(ruta)
+        for etiqueta, value in foto._getexif().items():
+            if etiqueta in ETIQUETAS:
+                datos[ETIQUETAS[etiqueta]] = value            
+        haveMta = True
+    except:
+        print ("\tERROR!\n\tPor favor revisa que la ruta sea correcta:\n",ruta,)
+        print ("\tAsegurate que el archivo este en formato JPG")
     for etiqueta, value in datos.items():
         key = ETIQUETAS.get(etiqueta, etiqueta)
         print ("\t-" + key + ": " + str(value))
