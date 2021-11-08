736d87becdf71dd7e9f4d49b4d250e6115785204
DoradoMx <47581649+DoradoMx@users.noreply.github.com>
2021-11-07 21:38:06 -0600

    mta_ruta
    Nueva función que no utiliza el explorador de archivos

--- PIAmetadatos.py
+++ PIAmetadatos.py
@@ -4,13 +4,12 @@ from PIL.ExifTags import TAGS as ETIQUETAS
 from tkinter.filedialog import askopenfilename
 
 def Metadatos():
-    print("Ejecutada la funcion")
     # Explorador de archivos
-    archivo_imagen = askopenfilename()
-    foto = Imagen.open(archivo_imagen)
     contiene_metadatos = False
     datos = {}
     try:
+        archivo_imagen = askopenfilename()
+        foto = Imagen.open(archivo_imagen)
         for etiqueta, value in foto._getexif().items():
             if etiqueta in ETIQUETAS:
                 datos[ETIQUETAS[etiqueta]] = value
@@ -21,6 +20,24 @@ def Metadatos():
     #print (datos)
     print("")
    
+    for etiqueta, value in datos.items():
+        key = ETIQUETAS.get(etiqueta, etiqueta)
+        print ("\t-" + key + ": " + str(value))
+
+def mta_ruta(ruta):
+    # Creamos una variable para avisar si hay o no metadatos
+    haveMta = False
+    datos = {}
+    try:
+        # Asignamos la ruta de la imagen JPG
+        foto = Imagen.open(ruta)
+        for etiqueta, value in foto._getexif().items():
+            if etiqueta in ETIQUETAS:
+                datos[ETIQUETAS[etiqueta]] = value            
+        haveMta = True
+    except:
+        print ("\tERROR!\n\tPor favor revisa que la ruta sea correcta:\n\t",ruta,)
+        print ("\t Asegurate que el archivo este en formato JPG")
     for etiqueta, value in datos.items():
         key = ETIQUETAS.get(etiqueta, etiqueta)
         print ("\t-" + key + ": " + str(value))
