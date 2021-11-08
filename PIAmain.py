33c078e5ad3a31e314e4baf0bc3daf515de70e35
Jorgestudent <88403071+Jorgestudent@users.noreply.github.com>
2021-11-08 14:37:05 -0600

    Add files via upload
    cambios con try except y logging

--- PIAmain.py
+++ PIAmain.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 
-import logging 
+import logging
 import sys
 import subprocess
 import argparse
+import configparser
 import PIAportscan as portscan
 import PIAanalyzer as analyzer
 #from PIAmetadatos import Metadatos as mta
+import PIAEmailSMS as emailsms
 
-logger = logging.getLogger( __name__ )
-logger.setLevel(level= logging.INFO)
-filehandler = logging.FileHandler("PIAmain.log")
-formatter = logging.Formatter("%(asctime)s: %(levelname)s - %(message)s")
+#Creamos el logger, establecemos niveles, archivo donde se guardara, y formato
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
+filehandler = logging.FileHandler('PIAmain.log')
+formatter = logging.Formatter( "%(asctime)s: %(levelname)s - %(message)s")
 filehandler.setFormatter(formatter)
 logger.addHandler(filehandler)
 
@@ -20,16 +23,21 @@ tools = ['Ps','Ua','EoS', 'Mta']
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-                                 description='''La suite PIA es una serie de herramientas usadas para cuestiones
-                                 de ciberseguridad''',
+                                 description='''The PIA framework is a set of tools used for security purposes,
+        consists of a portscaner, urlanalyzer, emails and sms sender,etc''',
                                  epilog = '''
     Dato: Para la funcion de Metadatos no se requieren argumentos
 
-    ejemplo para portscaner:
+    ejemplo para  Ps:
         PIAmain -t Ps -ip 192.168.15.0/24 -p 10-200 -S savehere.txt
 
-    ejemplo para Url analyzer:
-        PIAmain -t Ua -K (Tu key de VirusTotal) -U urls_sospechosas.txt''')
+    ejemplo para Ua:
+        PIAmain -t Ua -K (Tu key de VirusTotal) -U urls_sospechosas.txt
+
+    ejemplo para EoS:
+        PIAmain -t EoS -EoS email -e (nuestro correo) -co (contraseña) -re (correo de quien recibe) -a "aqui va el asunto" -b "mensaje"
+        PIAmain -t EoS -EoS sms -SID "Nuestro SID" -to "Nuestro token" -n "nuestro numero de Twilio" -d +52numero destino -m "mensaje"
+        ''')
 
 #choices=tools significa que solo acepta como valores elementos de la lista: tools
 parser.add_argument('-t', '--tool', type=str, metavar='', choices=tools, help=
@@ -45,11 +53,51 @@ Ua.add_argument('-K', '--Key', type=str, metavar='', help="Ingresa tu Key de Vir
 Ua.add_argument('-U', '--Urls', type=str, metavar='', help="Archivo con ulrs sospechosas")
 
 #Creamos el grupo EoS y agregamos los argumentos de correos o SMS a ese grupo
-#NOTA: los argumentos añadidos son solo de prueba
 EoS = parser.add_argument_group('Emails and SMS')
-EoS.add_argument('-e', '--email', type=str, metavar='', help="sender's email")
+EoS.add_argument('-EoS', type=str,
+                    help='Escribir que deseamos enviar: email, sms, ambos')
+# Parametros para email:
+EoS.add_argument('-e', '--email', type= str, metavar='',
+                    help="Ingresar nuestro correo")
+EoS.add_argument('-co', '--contra', type= str, metavar='',
+                    help="Ingresar nuestra contraseña")
+EoS.add_argument('-re', '--receiver', type= str, metavar='',
+                    help="Correo de quien recibira el email")
+EoS.add_argument('-a', '--asunto', type= str, metavar='',
+                    help="Ingresar el asunto del correo")
+EoS.add_argument('-b', '--body', type= str, metavar='',
+                    help="Ingresar el cuerpo del correo")
+EoS.add_argument('-f', '--file', default= 'no', metavar='',
+                    help="Especificar la ruta del archivo a adjuntar")
+# Parametros para sms:
+EoS.add_argument('-SID', metavar='',
+                    help="Ingresar nuestro SID de Twilio")
+EoS.add_argument('-to', metavar='',   
+                    help="Ingresar nuestra Token de Twilio")
+EoS.add_argument('-n','--number', metavar='',
+                    help="Ingresar nuestro numero de Twilio")
+EoS.add_argument('-d',    metavar='',  
+                    help="Ingresar el numero del destinatario(Agregar +52)")
+EoS.add_argument('-m', '--msj', metavar='',       
+                    help="Ingresar el mensaje a enviar")
+EoS.add_argument('--config', '-c', type=argparse.FileType('r'), help='config file')
+
 args = parser.parse_args()
 
+if args.config:
+        config = configparser.ConfigParser()
+        config.read_file(args.config)
+        args.email = config['EMAIL']['email']
+        args.contra = config['EMAIL']['contra']
+        args.receiver = config['EMAIL']['receiver']
+        args.asunto = config['EMAIL']['asunto']
+        args.body = config['EMAIL']['body']
+        args.file = config['EMAIL']['file']
+        args.SID = config['SMS']['SID']
+        args.token = config['SMS']['to']
+        args.number = config['SMS']['number']
+        args.numdest = config['SMS']['d']
+        args.msj = config['SMS']['msj']
 
 #Funcion tools, actualmente esta funcion no cumple ninguna funcion en el script.
 def tools():
@@ -79,68 +127,88 @@ def Pybanner():
 def banner():
     OS = sys.platform
     if OS == 'linux' or OS == 'darwin':
+        logger.info("Sistema UNIX-LIKE")
         try:
-            subprocess.run("./BASHbanner")
+             subprocess.run("./BASHbanner")
         except:
+            logger.info("ERROR al ejecutar BASHbanner, usando Pybanner")
             Pybanner()
     elif OS == 'win32':
+        logger.info("Sistema Windows detectado")
         try:
-            subprocess.run("./PSbanner.ps1")
+            subprocess.run("./PSbanner")
         except:
+            logger.info("ERROR al ejecutar PSbanner, usando Pybanner")
             Pybanner()
     else:
+        logger.info("No se pudo determinar OS")
         Pybanner()
 
 
 def main():
-    logger.info('corriendo main')
+    logger.info ('running main')
     # Ejecutando el portscanner
     if args.tool == 'Ps':
-        logger.info('Ps seleccionado')
+        logger.info ('portscaner seleccionado')
+        print ("Ps seleccionado")
         if not args.address == None:
             if args.save == False:
-                    print (banner())
+                    banner()
                     try:
-                        logger.info("ejecutando portscan.PortScan")
                         print (portscan.PortScan(args.address, args.port))
-                    except Exception as e:
-                        logger.info('Error al ejecutar portscan.PortScan', e)
-                        print (e)
-            else:   
+                    except:
+                        logger.info ("ERROR al ejecutar portscan.PortScan")
+                    
+            else:
                 try:
-                    logger.info("ejecutando portscan.Scansaver")
                     portscan.Scansaver(args.address, args.port, args.save)
-                except Exception as e:
-                    logger.info('Error al ejecutar portscan.Scansaver', e)
-                    print (e)
+                except:
+                    logger.info("ERROR al ejecutar portscan.Scansaver")
+                    
         else:
-            print ('DIRECCION IP NO DADA')
-            logger.info('no se dio ip')
+            print ('ADDRESS NOT GIVEN')
+
     # Fin de Portscanner
-    
     elif args.tool == 'Mta':
-        logger.info("Mta seleccionado")
-        banner()
+        logger.info('ejecutando metadatos')
+        print ("Metadatos seleccionado")
         try:
-            print ('Metadatos de una Imagen')
             mta()
         except:
-            logger.info("Error al ejecutar Mta()")
-            print ("ERROR")
+            logger.info("ERROR al ejecutar mta()")
 
+    elif args.tool == 'EoS':
+        logger.info("Emails y mensajes seleccionado")
+        print ('EoS Selected')
+        if args.EoS == 'email':
+            try:
+                 emailsms.send_emailP(args.email,args.contra,args.receiver,args.asunto,args.body,args.file)
+            except:
+                logging.info("ERROR al ejecutar emails.send_emailP")
+
+        elif args.EoS == 'sms':
+            try:
+                 emailsms.send_smsP(args.SID,args.to,args.number,args.d,args.msj)
+            except:
+                logging.info("emailsms.send_smsP")
+
+        elif args.EoS == 'ambos':
+            try:
+                 emailsms.send_emailP(args.email,args.contra,args.receiver,args.asunto,args.body,args.file)
+                 emailsms.send_smsP(args.SID,args.to,args.number,args.d,args.msj)
+            except:
+                logging.info("ERROR al ejecutar emailsms.send_emailP , email.send_smsP")
+            
     elif args.tool == 'Ua':
-        logger.info('Analizador de URLs seleccionado')
+        logging.info("Ua seleccionado")
+        print ("")
+        print ('Ua selected')
+        print ("")
         banner()
+        print ("")
         try:
-            logger.info("corriendo analyzer.inicio")
-            analyzer.inicio(args.Key, args.Urls)
-        except FileNotFoundError as e:
-            logger.info("Error al abrir archivo")
-            print (e)
+             analyzer.inicio(args.Key, args.Urls)
         except:
-            logger.info("Error al ejecutar analyzer.urls")
-            print ("Error al ejecutar\nEsta seguro que ingreso bien su API Key?")
-
+            logger.info("ERROR al ejecutar analyzer.inicio")
 
 main()
-
