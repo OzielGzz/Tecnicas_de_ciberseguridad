c53e454e46427d8acebfd899e329e18a8a1a7597
DoradoMx <47581649+DoradoMx@users.noreply.github.com>
2021-11-10 13:21:02 -0600

    Metadatos
    Me di cuenta de que había una variable que no se usaba, la hice función por la nostalgia

--- PIAmetadatos.py
+++ PIAmetadatos.py
@@ -3,6 +3,13 @@ from PIL import Image as Imagen
 from PIL.ExifTags import TAGS as ETIQUETAS
 from tkinter.filedialog import askopenfilename
 
+#Esta funcion sobra mucho, pero la dejo por la nostalgia
+def haveMta(existen):
+    if (existen == True):
+        print ("\tMetadatos encontrados:")
+    else:
+        print ("\tNo hay metadatos encontrados")
+
 def Metadatos():
     contiene_metadatos = False
     datos = {}
@@ -18,14 +25,17 @@ def Metadatos():
     except:
         print ("\tERROR!\n\tPor favor revisa que la ruta sea correcta:\n",archivo_imagen,)
         print ("\t Asegurate que el archivo este en formato JPG")
-   
+    #Imprimimos en pantalla los metadatos
+    haveMta(contiene_metadatos) 
     for etiqueta, value in datos.items():
         key = ETIQUETAS.get(etiqueta, etiqueta)
         print ("\t-" + key + ": " + str(value))
+        
+
 
 def mta_ruta(ruta):
     # Creamos una variable para avisar si hay o no metadatos
-    haveMta = False
+    contiene_metadatos = False
     datos = {}
     try:
         # Asignamos la ruta de la imagen JPG
@@ -33,10 +43,13 @@ def mta_ruta(ruta):
         for etiqueta, value in foto._getexif().items():
             if etiqueta in ETIQUETAS:
                 datos[ETIQUETAS[etiqueta]] = value            
-        haveMta = True
+        contiene_metadatos = True
     except:
         print ("\tERROR!\n\tPor favor revisa que la ruta sea correcta:\n",ruta,)
         print ("\tAsegurate que el archivo este en formato JPG")
+    #Imprimimos en pantalla los metadatos
+    haveMta(contiene_metadatos) 
     for etiqueta, value in datos.items():
         key = ETIQUETAS.get(etiqueta, etiqueta)
         print ("\t-" + key + ": " + str(value))
+    
